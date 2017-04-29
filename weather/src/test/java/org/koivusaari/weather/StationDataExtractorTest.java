3fc3187d539703852b3932ba6e495e0315a08e1d
Mika Koivusaari <mika.koivusaari@gmail.com>
2017-04-29 22:55:29 +0300

    Added some tests.

--- weather/src/test/java/org/koivusaari/weather/StationDataExtractorTest.java
+++ weather/src/test/java/org/koivusaari/weather/StationDataExtractorTest.java
@@ -1,16 +1,35 @@
 package org.koivusaari.weather;
 
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+import org.koivusaari.weather.pojo.StationData;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 import junit.framework.TestCase;
 
 public class StationDataExtractorTest extends TestCase {
 
 	private StationDataExtractor ste;
+	@Mock
+	private ResultSet rs;
+	@Mock
+	private ResultSetMetaData rsmt;
 	
 	public StationDataExtractorTest(String name) {
 		super(name);
 	}
 
 	protected void setUp() throws Exception {
+	    MockitoAnnotations.initMocks(this);
 		ste=new StationDataExtractor();
 		super.setUp();
 	}
@@ -20,5 +39,27 @@ public class StationDataExtractorTest extends TestCase {
 	}
 	public void testGenerateMethodNameUnderscore(){
 		assertEquals("setAirPressure", ste.generateMethodName("AIR_PRESSURE"));
+	}
+	
+	@SuppressWarnings("deprecation")
+	public void testExtractDataDate() throws SQLException{
+	    when(rs.getMetaData()).thenReturn(rsmt);
+	    when(rs.next()).thenReturn(true);
+	    when(rsmt.getColumnCount()).thenReturn(1);
+	    when(rs.getObject(1)).thenReturn(new Date(117, 0, 1, 12, 10));
+	    when(rsmt.getColumnLabel(1)).thenReturn("TIME");
+	    
+	    StationData st=ste.extractData(rs);
+	    assertEquals(LocalDateTime.of(2017, 1, 1, 12, 10), st.getTime());
+	}
+	public void testExtractDataNumber() throws SQLException{
+	    when(rs.getMetaData()).thenReturn(rsmt);
+	    when(rs.next()).thenReturn(true);
+	    when(rsmt.getColumnCount()).thenReturn(1);
+	    when(rs.getObject(1)).thenReturn(new BigDecimal(1.1));
+	    when(rsmt.getColumnLabel(1)).thenReturn("TEMPERATURE");
+	    
+	    StationData st=ste.extractData(rs);
+	    assertEquals(new Float(1.1), st.getTemperature());
 	}
 }
