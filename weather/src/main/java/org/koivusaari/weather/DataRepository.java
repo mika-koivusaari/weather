73394d011131f4df18cadb20e12d5e740ce565d0
Mika Koivusaari <mika.koivusaari@gmail.com>
2017-05-02 14:13:31 +0300

    Disable rest.

--- weather/src/main/java/org/koivusaari/weather/DataRepository.java
+++ weather/src/main/java/org/koivusaari/weather/DataRepository.java
@@ -3,7 +3,9 @@ package org.koivusaari.weather;
 import org.koivusaari.weather.pojo.Data;
 import org.koivusaari.weather.pojo.DataPK;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(exported = false)
 public interface DataRepository extends CrudRepository<Data, DataPK>{
 
 }
