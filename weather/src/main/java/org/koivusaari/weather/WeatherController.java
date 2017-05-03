e8d7ca9854e2bbdc9c78183772f8023b2415ad5f
Mika Koivusaari <mika.koivusaari@gmail.com>
2017-05-03 08:59:31 +0300

    Added google analytics.

--- weather/src/main/java/org/koivusaari/weather/WeatherController.java
+++ weather/src/main/java/org/koivusaari/weather/WeatherController.java
@@ -4,6 +4,7 @@ import org.koivusaari.weather.pojo.WeatherData;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,8 @@ public class WeatherController {
 
 	@Autowired
 	private TemplateResolver templateResolver;
+	@Value("${google.analytics.id:#{null}}")
+	private String analId;
 	
 	private WeatherRepository weatherRepository;
 	private static final Logger log = LoggerFactory.getLogger(WeatherController.class);
@@ -28,6 +31,7 @@ public class WeatherController {
 		WeatherData weatherData=weatherRepository.findLastData();
 		log.debug("WeatherData: "+weatherData);
         model.addAttribute("weatherdata", weatherData);
+        model.addAttribute("analId", analId);
 
         templateResolver.initialize();
         log.debug(templateResolver.toString());
